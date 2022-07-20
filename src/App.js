2acbb88a1f7618881aef1d847a226a55226bfef4
Halil Olcay <holcay92@gmail.com>
2022-07-20 14:54:39 +0300

    readme file and info file added

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ import SearchIcon from './search.svg';
 
 const API_URL = 'http://www.omdbapi.com?apikey=2d451bba';
 
-const movie ={
+/*const movie ={
     "Title": "Superman, Spiderman or Batman",
     "Year": "2011",
     "imdbID": "tt2084949",
     "Type": "movie",
     "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ4MzcxNDU3N15BMl5BanBnXkFtZTgwOTE1MzMxNzE@._V1_SX300.jpg"
-}
+}*/
 const App =() => {
 
     const [movies, setMovies] = useState([]);
